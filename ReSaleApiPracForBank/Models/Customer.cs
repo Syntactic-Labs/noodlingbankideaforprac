8ac68d89e509140aa7fb899e3ea70417cf48adde
Michael Walker <walkerrepair255@yahoo.com>
2021-12-13 12:20:11 -0500

    clearing school computer

---
+++ ReSaleApiPracForBank/Models/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReSaleApiPracForBank.Models
+{
+    public class Customer
+    {
+
+        public Customer() { }
+
+        public int Id { get; set; }
+        [StringLength(30), Required]
+        public string Username { get; set; }
+        [StringLength(30), Required]
+        public string Password { get; set; }
+        [StringLength(30), Required]
+        public string Firstname { get; set; }
+        [StringLength(30), Required]
+        public string Lastname { get; set; }
+        [StringLength(30), Required]
+        public string Address { get; set; }
+        [StringLength(30), Required]
+        public string City { get; set; }
+        [StringLength(2), Required]
+        public string State { get; set; }
+        [StringLength(5), Required]
+        public string Zip { get; set; }
+        [StringLength(14)]
+        public string Phone { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+        
+    }
+}
---
+++ ReSaleApiPracForBank/Models/PracDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReSaleApiPracForBank.Models
+{
+    public class PracDbContext : DbContext
+    {
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public PracDbContext(DbContextOptions<PracDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
